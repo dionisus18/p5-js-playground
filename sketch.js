42d435fae14282862ac438255d57d30a7f4e1f3f
Kevin <kevin.gonzalezr@hotmail.es>
2018-10-25 22:47:54 -0300

    init

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+    print("hello");
+}
+
+function draw() {
+    background(210);
+
+    rectMode(CENTER);
+    fill(0,0,255);
+    stroke(100, 0 , 25,50);
+    strokeWeight(50);
+    rect(150, 150, 55, 55);
+}
+
