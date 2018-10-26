0de9c339e48a48f8ee4493129a843700905b5262
Kevin <kevin.gonzalezr@hotmail.es>
2018-10-25 23:10:23 -0300

    Reordenamiento y dependecias locales

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,13 @@
 function setup() {
     createCanvas(400, 400);
-    print("hello");
+    background(210);
 }
 
 function draw() {
-    background(210);
-
     rectMode(CENTER);
     fill(0,0,255);
     stroke(100, 0 , 25,50);
     strokeWeight(50);
-    rect(150, 150, 55, 55);
+    rect(mouseX, mouseY, 55, 55);
 }
 
